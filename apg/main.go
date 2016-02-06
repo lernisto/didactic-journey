456cbfc6ca476d654ad0cf7751bfdb26e14a25d6
Terrel Shumway <terrel@gmail.com>
2016-02-06 03:26:16 -0700

    sweet: "%x" does hex encoding of strings/[]byte
    Also: always gofmt before checkin. I will learn how to set up a hook

--- apg/main.go
+++ apg/main.go
@@ -1,31 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"crypto/rand"
-	"io"
-	"encoding/base64"
-	"encoding/base32"
 	"encoding/ascii85"
-	"encoding/hex"
+	"encoding/base32"
+	"encoding/base64"
+	"fmt"
+	"io"
 	"os"
 )
 
-
 func main() {
-	bpasswd := make([]byte,16)
-	_,e := io.ReadFull(rand.Reader,bpasswd)
-	if(e!=nil){
+	bpasswd := make([]byte, 16)
+	_, e := io.ReadFull(rand.Reader, bpasswd)
+	if e != nil {
 		fmt.Println(e)
 	}
 
-	p0 := hex.EncodeToString(bpasswd)
 	p1 := base32.StdEncoding.EncodeToString(bpasswd)
 	p2 := base64.StdEncoding.EncodeToString(bpasswd)
-	
-	encoded := make([]byte,32)
-	l := ascii85.Encode(encoded,bpasswd)
-	
-	fmt.Printf("%s\n%s\n%s\n%s\n", p0,p1,p2,encoded[:l] )
+
+	encoded := make([]byte, 32)
+	l := ascii85.Encode(encoded, bpasswd)
+
+	fmt.Printf("%x\n%s\n%s\n%s\n", bpasswd, p1, p2, encoded[:l])
 	fmt.Println(os.Args)
 }
